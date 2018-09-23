852ac9802497e92c80fc70621319499e82d5ef59
Abid Ali <abdsoftfsd@gmail.com>
2018-09-23 22:47:25 +0500

    updates package.json and  readme.md files

--- app/tests/todo.spec.ts
+++ app/tests/todo.spec.ts
@@ -1,19 +1,10 @@
-// import "mocha";
-// import Todo_Model from "../app/Models/Todo.model";
-// import * as assert from "assert";
-// import * as chai from 'chai';
 import serverConfig from "../serverConfig";
-import * as mongoose from "mongoose";
 import Todo from "../Models/Todo.model";
 const url = serverConfig.apiUrl;
-// const server = require("../server"); // server must be running before performing these tests
 const app = require("../server");
 const request = require("supertest");
-const should = require("should");
-// dynamic id generation creates problem, it stores 1 character ahead in database that is why,
-// the delete request failed every time.
-// const testTodoId = mongoose.Types.ObjectId();
-//  so the solution is to hardcode a generated id into the code for testing purpose.
+
+// a demo static id to create a new todo and then delete it in the testing.
 const testTodoId = "5ba7b664750cf529f001a2d1";
 let testTodo = {
   _id: testTodoId,
@@ -22,10 +13,6 @@ let testTodo = {
   done: false
 };
 describe("Todo Rest API Tests", () => {
-  // beforeAll(async () => {
-  //   let newTodo = new Todo(testTodo);
-  //   await newTodo.save();
-  // });
 
   // Create a new Todo
   describe('Create a new Todo', () => {
